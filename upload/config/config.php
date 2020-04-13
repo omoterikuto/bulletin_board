0acbfbed034d51e96c95cead565d9a017c58a222
omoterikuto <52554440+omoterikuto@users.noreply.github.com>
2020-04-13 18:01:29 +0900

    Update config.php

--- upload/config/config.php
+++ upload/config/config.php
@@ -2,13 +2,13 @@
 
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:host=mysql141.phy.lolipop.lan;dbname=LAA1104681-upload;charset=utf8');
-define('DB_USERNAME', 'LAA1104681');
-define('DB_PASSWORD', 'Roim0624');
+define('DSN', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
 define('ADMIN_PASSWORD', 'password');
 
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
-define('HOME_DIR',  SITE_URL . '/works/upload/public_html');
+define('HOME_DIR',  SITE_URL . '/upload/public_html');
 define('MAX_FILE_SIZE', 100 * 1024 * 1024); // 100MB
 define('THUMBNAIL_WIDTH', 400);
 define('IMAGES_DIR', __DIR__ . '/../images');
